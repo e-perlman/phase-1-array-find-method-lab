ecb06e1b1b5fdf3771a907d1a1c5eec5e70b06ea
Emma Perlman <emmajperlman@gmail.com>
2022-02-15 20:30:00 -0800

    commit tree d3ca3e6c6e323b09466a64a869f3644b6dd2313c

--- index.js
+++ index.js
@@ -1,1 +1,6 @@
-// code your solution here
+function superbowlWin(array){
+    const win=array.find(element => element.result==='W' )
+    if (typeof win==='object'){
+        return win.year
+    }
+}
